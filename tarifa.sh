9cd743044be4cb0388a391eea86df5512e0dd750
JorgitoR72 <joreju@floridauniversitaria.es>
2023-02-09 10:15:41 +0100

    tarifa

--- tarifa.sh
+++ tarifa.sh
@@ -2,10 +2,10 @@
 if [ $1 -gt 50 ];then
     coste=100
     echo "Se recomienda:- Tarifa 1: Importe es: $coste por $1 llamadas"
-elif [ $1 -gt 10 ] && [ $1 -lt 50 ];then
-    coste=$((50 + ($1 * 1)))git
+elif [ $1 -gt 10 ] && [ $1 -lt 51 ];then
+    coste=$((50+$1*1))
     echo "Se recomienda:- Tarifa 2: Importe es: $coste por $1 llamadas"
 else
-    coste=$((20 + ($1 * 2)))
+    coste=$((20+$1*2))
     echo "Se recomienda:- Tarifa 3: Importe es: $coste por $1 llamadas"
 fi
