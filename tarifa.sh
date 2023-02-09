d4212a449113e8ebeedefaced99b9c171a88780e
JorgitoR72 <joreju@floridauniversitaria.es>
2023-02-09 10:01:28 +0100

    tarifa

---
+++ tarifa.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+if [ $1 -gt 50 ];then
+    coste=100
+    echo "Se recomienda:- Tarifa 1: Importe es: $coste por $1 llamadas"
+elif [ $1 -gt 10 ] && [ $1 -lt 50 ];then
+    coste=$((50 + ($1 * 1)))git
+    echo "Se recomienda:- Tarifa 2: Importe es: $coste por $1 llamadas"
+else
+    coste=$((20 + ($1 * 2)))
+    echo "Se recomienda:- Tarifa 3: Importe es: $coste por $1 llamadas"
+fi
