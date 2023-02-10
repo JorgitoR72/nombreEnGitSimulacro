0ea75d10f3346a5b74ad628cd45d7177848e090f
JorgitoR72 <joreju@floridauniversitaria.es>
2023-02-10 22:57:11 +0100

    d

---
+++ d.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Error: debes pasar una nota numérica como parámetro"
+  exit 1
+fi
+
+nota=$1
+
+if [ $nota -lt 5 ]; then
+  echo "Suspendido"
+elif [ $nota -ge 5 ] && [ $nota -lt 8 ]; then
+  echo "Aprobado"
+else
+  echo "Sobresaliente"
+fi
